3715221cf40acf13f63e74a8aff7ed42cfd51bea
sho.kuroda <sho.kuroda@l-is-b.com>
2023-06-16 20:50:01 +0900

    style: format

--- src/direct.js
+++ src/direct.js
@@ -9,7 +9,7 @@
 
 // customize
 let hubot;
-const endpoint = process.env.HUBOT_DIRECT_ENDPOINT != null ? process.env.HUBOT_DIRECT_ENDPOINT : "wss://api.direct4b.com/albero-app-server/api";
+const endpoint = process.env.HUBOT_DIRECT_ENDPOINT != null ? process.env.HUBOT_DIRECT_ENDPOINT : 'wss://api.direct4b.com/albero-app-server/api';
 const accessToken = process.env.HUBOT_DIRECT_TOKEN;
 const proxyURL = process.env.HUBOT_DIRECT_PROXY_URL || process.env.HTTPS_PROXY || process.env.HTTP_PROXY;
 const talkWithBot = process.env.HUBOT_DIRECT_TALKWITHBOT;
@@ -17,10 +17,10 @@ const storage_path = process.env.HUBOT_DIRECT_STORAGE_PATH;
 const storage_quota = process.env.HUBOT_DIRECT_STORAGE_QUOTA;
 const ws_config = (() => { try { return JSON.parse(process.env.HUBOT_DIRECT_WS_CONFIG); } catch (error) {} })();
 const offline = process.env.HUBOT_DIRECT_OFFLINE;
-const initTimeout = Number(process.env.HUBOT_DIRECT_INIT_TIMEOUT) || 0; //s
+const initTimeout = Number(process.env.HUBOT_DIRECT_INIT_TIMEOUT) || 0; // s
 
 // Hubot dependencies
-const {Adapter,TextMessage,EnterMessage,LeaveMessage,JoinMessage,TopicMessage} = require('lisb-hubot/es2015');
+const { Adapter, TextMessage, EnterMessage, LeaveMessage, JoinMessage, TopicMessage } = require('lisb-hubot/es2015');
 
 // dependencies
 const {
@@ -29,65 +29,64 @@ const {
 const {
   DirectAPI
 } = require('direct-js');
-const url          = require('url');
+const url = require('url');
 
 class Direct extends Adapter {
-
-  send(envelope, ...strings) {
+  send (envelope, ...strings) {
     return strings.forEach(string => {
-      if (typeof(string) === 'function') {
+      if (typeof (string) === 'function') {
         return string();
       } else {
         if (envelope.user != null) {
-          this.robot.logger.debug("Sending strings to user: " + envelope.user.name);
+          this.robot.logger.debug('Sending strings to user: ' + envelope.user.name);
         }
         return this.bot.send(envelope, string);
       }
     });
   }
 
-  reply(envelope, ...strings) {
+  reply (envelope, ...strings) {
     if (envelope.user != null) {
       return this.send(envelope, ...Array.from(strings.map(str => `@${envelope.user.name} ${str}`)));
     }
   }
 
-  announce(envelope, ...strings) {
+  announce (envelope, ...strings) {
     return Array.from(strings).map((string) =>
       this.bot.announce(envelope, string));
   }
 
-  topic(envelope, ...strings) {
+  topic (envelope, ...strings) {
     return this.bot.topic(envelope, strings.join(','));
   }
 
-  download(envelope, remoteFile, callback) {
+  download (envelope, remoteFile, callback) {
     return this.bot.download(envelope, remoteFile, callback);
   }
 
-  leave(envelope, user) {
+  leave (envelope, user) {
     return this.bot.leave(envelope, user);
   }
 
-  users(domainId) {
+  users (domainId) {
     return this.bot.userObjects(domainId);
   }
 
-  talks() {
+  talks () {
     return this.bot.talkObjects();
   }
 
-  domains() {
+  domains () {
     return this.bot.domainObjects();
   }
 
-  run() {
+  run () {
     const self = this;
 
     const options = {
-      host:     url.parse(endpoint).host,
+      host: url.parse(endpoint).host,
       endpoint,
-      name:     this.robot.name,
+      name: this.robot.name,
       access_token: accessToken,
       proxyURL,
       talkWithBot,
@@ -105,52 +104,52 @@ class Direct extends Adapter {
         userObjects: () => { return {}; },
         talkObjects: () => { return {}; },
         domainObjects: () => { return {}; },
-        setOptions: () => {  },
-        on: () => {  },
-        listen: () => setTimeout(() => self.emit("connected", 1000))
+        setOptions: () => { },
+        on: () => { },
+        listen: () => setTimeout(() => self.emit('connected', 1000))
       };
     }
 
     bot.setOptions(options);
 
-    const withAuthor = callback => (function(talk, user, msg) {
+    const withAuthor = callback => function (talk, user, msg) {
       let key, value;
       const envelope = {};
       for (key in user) { value = user[key]; envelope[key] = value; }
       for (key in talk) { value = talk[key]; envelope[key] = value; }
       return callback(envelope, msg);
-    });
+    };
 
-    bot.on("TextMessage",
+    bot.on('TextMessage',
       withAuthor((envelope, msg) => self.receive(new TextMessage(envelope, msg.content, msg.id, msg.mention)))
     );
 
-    bot.on("EnterMessage",
+    bot.on('EnterMessage',
       withAuthor((envelope, msg) => self.receive(new EnterMessage(envelope, null, msg.id)))
     );
 
-    bot.on("LeaveMessage",
+    bot.on('LeaveMessage',
       withAuthor((envelope, msg) => self.receive(new LeaveMessage(envelope, null, msg.id)))
     );
 
-    bot.on("JoinMessage",
+    bot.on('JoinMessage',
       withAuthor((envelope, msg) => self.receive(new JoinMessage(envelope, null, null)))
     );
 
-    bot.on("TopicChangeMessage",
+    bot.on('TopicChangeMessage',
       withAuthor((envelope, topic) => self.receive(new TopicMessage(envelope, topic, null)))
     );
 
-    bot.on("error_occurred", function(err, obj) {
-      for (var key in obj) { var value = obj[key]; err[key] = value; }
-      return self.robot.emit("error", err);
+    bot.on('error_occurred', function (err, obj) {
+      for (const key in obj) { const value = obj[key]; err[key] = value; }
+      return self.robot.emit('error', err);
     });
 
-    bot.on("init_timeout", () => self.robot.emit("error", new Error("Initialize timeout")));
+    bot.on('init_timeout', () => self.robot.emit('error', new Error('Initialize timeout')));
 
-    bot.on("data_recovered", function() {
+    bot.on('data_recovered', function () {
       clearTimeout(initTimer);
-      return self.emit("connected");
+      return self.emit('connected');
     });
 
     bot.listen();
@@ -160,9 +159,9 @@ class Direct extends Adapter {
 
     if (initTimeout > 0) {
       var initTimer;
-      const timeoutBehavior = function() {
-        self.emit("connected"); // load scripts
-        return bot.emit("init_timeout");
+      const timeoutBehavior = function () {
+        self.emit('connected'); // load scripts
+        return bot.emit('init_timeout');
       };
       return initTimer = setTimeout(timeoutBehavior, initTimeout * 1000);
     }
